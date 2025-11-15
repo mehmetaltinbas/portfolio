2d87e25c4cad2912cd2e6bc71e3e42a9b3a78251
altnbsmehmet <altnbsmehmet@icloud.com>
2025-11-15 14:48:21 +0300

    next.config update

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,9 @@
 import type { NextConfig } from 'next';
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+    experimental: {
+        serverComponentsExternalPackages: ["@prisma/client", "prisma"],
+    },
+};
 
 export default nextConfig;
